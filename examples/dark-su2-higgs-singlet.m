981e0723ef2a524e2a2fa0a77df0e89ae136e30c
Philipp Schicho <schicho@icloud.com>
2024-06-06 14:38:49 +0200

    tidy the dark su2 higgs singlet model file

--- examples/dark-su2-higgs-singlet.m
+++ examples/dark-su2-higgs-singlet.m
@@ -1,6 +1,6 @@
 (* ::Package:: *)
 
-Quit[]
+(*Quit[];*)
 
 
 SetDirectory[NotebookDirectory[]];
@@ -16,7 +16,7 @@ $LoadGroupMath=True;
 This is a DRalgo model file for the
 SU(2) + doublet + singlet dark sector 'toy model' used in appendix C of
 "Cosmological phase transitions at three loops: the final verdict on perturbation theory"
-by Andreas Ekstedt, Philipp Schicho and Tuomas V. I. Tenkanen, [2405.xxxxx]. 
+by Andreas Ekstedt, Philipp Schicho and Tuomas V. I. Tenkanen, [2405.18349]. 
 
 The model is a simplified toy setup, where only
 the SU(2) gauge fields and doublet and singlet scalars are added.
@@ -34,42 +34,102 @@ see e.g. DRalgo model file sm.m.
 	Running this cell defines the model.
 	For more documentation, see the DRalgo manual [2205.08815].
 *)
-Group={"SU2"}; (* SU(2) gauge group *)
-RepAdjoint={{2}}; (* gauge field in adjoint representation (rep) *)
-Doublet={{{1}},"C"}; (* scalar in fundamental rep *)
-Singlet={{{0}},"R"}; (* real scalar, in trivial rep *)
-RepScalar={Doublet,Singlet}; 
-CouplingName={g}; (* gauge coupling *)
+(* SU(2) gauge group *)
+Group={"SU2"};
+(* gauge field in adjoint representation (rep) *)
+RepAdjoint={{2}};
+(* scalar in fundamental rep *)
+Doublet={{{1}},"C"};
+(* real scalar, in trivial rep *)
+Singlet={{{0}},"R"};
+RepScalar={Doublet,Singlet};
+(* gauge coupling *)
+CouplingName={g};
+
 
 (* no fermions *)
 RepFermion = {}; 
 
-(* allocating tensor structures: *)
+
+(* ::Text:: *)
+(*The input for the gauge interactions to DRalgo are then given by*)
+
+
 {gvvv,gvff,gvss,\[Lambda]1,\[Lambda]3,\[Lambda]4,\[Mu]ij,\[Mu]IJ,\[Mu]IJC,Ysff,YsffC}=AllocateTensors[Group,RepAdjoint,CouplingName,RepFermion,RepScalar];
 
-(* construct scalar group invariants for scalar potential: *)
+
+(* ::Text:: *)
+(*Define scalar group invariants for scalar potential:*)
+
+
 InputInv={{1,1},{True,False}};
 MassTerm1=CreateInvariant[Group,RepScalar,InputInv]//Simplify//FullSimplify;
 InputInv={{2,2},{True,True}};
 MassTerm2=CreateInvariant[Group,RepScalar,InputInv]//Simplify//FullSimplify;
 
+
 (* mass terms *)
-VMass=(m\[Phi]sq*MassTerm1+1/2 mssq*MassTerm2); (* here m\[Phi]sq = m\[Phi]^2 and  mssq = ms^2 *)
+(* here m\[Phi]sq = m\[Phi]^2 and  mssq = ms^2 *)
+VMass=(m\[Phi]sq*MassTerm1+1/2 mssq*MassTerm2); 
 \[Mu]ij=GradMass[VMass[[1]]]//Simplify//SparseArray;
 
-(* quartic terms *)
+
+(* ::Text:: *)
+(*Add quartic operators:*)
+
+
 QuarticTerm1=MassTerm1[[1]]^2;
 QuarticTerm2=MassTerm2[[1]]^2;
 QuarticTerm3=MassTerm1[[1]]*MassTerm2[[1]];
+
 
 VQuartic=(
 	+\[Lambda]\[Phi]*QuarticTerm1 (* doublet self-interaction *)
 	+\[Lambda]s/4*QuarticTerm2 (* singlet self-interaction *)
 	+\[Lambda]m/2*QuarticTerm3 (* quartic portal interaction *)
 	);
-	
+
+
 \[Lambda]4=GradQuartic[VQuartic];
-	
+
+
+(* ::Text:: *)
+(*Add cubic operators:*)
+
+
+InputInv={{1,1,2},{True,False,True}};
+CubicTerm1=CreateInvariant[Group,RepScalar,InputInv][[1]]//Simplify;
+InputInv={{2,2,2},{True,True,True}};
+CubicTerm2=CreateInvariant[Group,RepScalar,InputInv][[1]]//Simplify;
+
+
+VCubic=(
+	+\[Mu]m/2*CubicTerm1
+	+\[Mu]3/3*CubicTerm2
+	);
+
+
+\[Lambda]3=GradCubic[VCubic];
+
+
+(* ::Text:: *)
+(*Add tadpole operator:*)
+
+
+InputInv={{2},{True}};
+TadpoleTerm1=CreateInvariant[Group,RepScalar,InputInv][[1]]//Simplify;
+
+
+VTadpole=\[Mu]1*TadpoleTerm1;
+
+
+\[Lambda]1=GradTadpole[VTadpole];
+
+
+(* ::Text:: *)
+(*Add some dimension six operators:*)
+
+
 (*
 The leading marginal operators could be included by defining following Dim6Term,
 and by using below the ImportModelDRalgo with option Mode -> 3.
@@ -84,49 +144,30 @@ Dim6Term=(
 \[Lambda]6=GradSextic[Dim6Term];
 *)
 
-(*
-define group invariants for cubic operators:
-*)
-InputInv={{1,1,2},{True,False,True}};
-CubicTerm1=CreateInvariant[Group,RepScalar,InputInv][[1]]//Simplify;
-InputInv={{2,2,2},{True,True,True}};
-CubicTerm2=CreateInvariant[Group,RepScalar,InputInv][[1]]//Simplify;
-
-(* and add cubic operators: *)
-VCubic=(
-	+\[Mu]m/2*CubicTerm1
-	+\[Mu]3/3*CubicTerm2
-	);
-
-\[Lambda]3=GradCubic[VCubic];
-
-(* add tadpole: *)
-InputInv={{2},{True}};
-TadpoleTerm1=CreateInvariant[Group,RepScalar,InputInv][[1]]//Simplify;
-
-VTadpole=\[Mu]1*TadpoleTerm1;
-
-\[Lambda]1=GradTadpole[VTadpole];
-
 
 (* ::Section:: *)
 (*Dimensional Reduction*)
 
 
-(* import model defined above *)
-ImportModelDRalgo[Group,gvvv,gvff,gvss,\[Lambda]1,\[Lambda]3,\[Lambda]4,\[Mu]ij,\[Mu]IJ,\[Mu]IJC,Ysff,YsffC,Verbose->False(*,Mode->3*)]; 
-(* Mode -> 3 option is used for marginal operators if desired, in this case one also includes: *)
-(*DefineDim6[\[Lambda]6]*)
-
-
-
 (* ::Subsection:: *)
-(*Hard to soft EFT construction*)
+(*Model Import*)
 
 
-(*
-'hard to soft' EFT construction
-*)
+ImportModelDRalgo[Group,gvvv,gvff,gvss,\[Lambda]1,\[Lambda]3,\[Lambda]4,\[Mu]ij,\[Mu]IJ,\[Mu]IJC,Ysff,YsffC,Verbose->False]; 
+
+
+(* ::Text:: *)
+(* Mode -> 3 option is used for marginal operators if desired, in this case one also includes:*)
+
+
+(*ImportModelDRalgo[Group,gvvv,gvff,gvss,\[Lambda]1,\[Lambda]3,\[Lambda]4,\[Mu]ij,\[Mu]IJ,\[Mu]IJC,Ysff,YsffC,Verbose->False,Mode->3];
+DefineDim6[\[Lambda]6]*)
+
+
+(* ::Subsection::Closed:: *)
+(*Hard-to-soft EFT construction*)
+
+
 PerformDRhard[] 
 
 
@@ -137,46 +178,59 @@ Lb = Log[\[Mu]^2/T^2]+2*EulerGamma-2*Log[4\[Pi]];
 PrintConstants[]
 
 
+(* ::Subsubsection::Closed:: *)
+(*Couplings  in  the  3D EFT*)
+
+
 (*
-print the 3D couplings in the EFT in terms of
+The 3D couplings are functions of
 temperature T and parameters of the parent theory
 *)
 PrintCouplings[]
 
 
-(*
-couplings to temporal gauge field components
-*)
+(* ::Subsubsection::Closed:: *)
+(*Couplings to temporal gauge field components:*)
+
+
 PrintTemporalScalarCouplings[]
 
 
+(* ::Subsubsection::Closed:: *)
+(*Debye  masses  of  temporal  gauge  field  component  to 2-loop  order*)
+
+
 (*
-Debye masses of temporal gauge field component to
-2-loop order
 (here \[Mu]sqSU2 == mD^2)
 *)
 PrintDebyeMass["LO"]
 PrintDebyeMass["NLO"]
 
 
+(* ::Subsubsection::Closed:: *)
+(*Print 3D masses in the EFT*)
+
+
 (*
-print 3D masses in the EFT,i.e. thermal masses,
+These are the thermal masses,
 in terms of temperature T and parameters of the parent theory
 *)
 PrintScalarMass["LO"] (* one-loop thermal masses *)
 PrintScalarMass["NLO"] (* two-loop correction to thermal masses *)
 
 
-(*
-singlet tadpole to two-loops:
-*)
+(* ::Subsubsection::Closed:: *)
+(*Singlet tadpole to 2-loops:*)
+
+
 PrintTadpoles["LO"] 
 PrintTadpoles["NLO"]
 
 
-(* =
-parent theory beta functions at one-loop:
-*)
+(* ::Subsubsection::Closed:: *)
+(*1-loop parent  theory  beta  functions:*)
+
+
 (*
 Notation defined as
 beta[g^2] = g^2 /. g^2\[Rule] -((43 g^4)/(48 \[Pi]^2))
@@ -185,8 +239,12 @@ etc.
 BetaFunctions4D[]
 
 
+(* ::Subsubsection::Closed:: *)
+(*Unit operator pE:*)
+
+
 (*
-unit operator pE, or hard mode contribution to symmetric phase pressure
+this is the hard mode contribution to symmetric phase pressure
 *)
 PrintPressure["LO"]
 PrintPressure["NLO"] 
@@ -202,34 +260,40 @@ PosFermion=PrintFermionRepPositions[]*)
 
 
 (* ::Subsection:: *)
-(*Soft to softer EFT construction*)
+(*Soft-to-softer EFT construction*)
 
 
 (*
-'soft to softer' EFT construction,
 by integrating out temporal gauge field components, and
 the singlet (scalar list index 5)
 *)
 PerformDRsoft[{5}];
 
 
-(*
-scalar and gauge couplings in 'softer' 3D EFT:
-*)
+(* ::Subsubsection::Closed:: *)
+(*Scalar and gauge couplings in softer 3D EFT:*)
+
+
 PrintCouplingsUS[]
 
 
-(*
-scalar 3d mass in 'softer' 3D EFT:
-*)
+(* ::Subsubsection::Closed:: *)
+(*Scalar 3D mass in softer 3D EFT:*)
+
+
 PrintScalarMassUS["LO"]
 PrintScalarMassUS["NLO"]
 
 
-(*
-the running of mass in 3d is governed by
-*)
+(* ::Subsubsection::Closed:: *)
+(*Softer 3D EFT beta functions:*)
+
+
 BetaFunctions3DUS[]
+
+
+(* ::Subsubsection:: *)
+(*The  unit  operator  pM:*)
 
 
 (*
@@ -237,9 +301,11 @@ soft mode constributions to unit operator (pM)
 *)
 PrintPressureUS["LO"] 
 PrintPressureUS["NLO"]
+
 (*
 Currently, the "NNLO" part cannot be computed by DRalgo
 *) 
+PrintPressureUS["NNLO"]
 
 
 
