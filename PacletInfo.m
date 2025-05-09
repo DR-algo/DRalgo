d3477a0b578f7d33f567eb27df47bc2f36f5aa1c
Philipp Schicho <schicho@icloud.com>
2025-05-10 00:14:07 +0200

    update paclet info

--- PacletInfo.m
+++ PacletInfo.m
@@ -2,23 +2,28 @@
 
 PacletObject[
   <|
-    "Name" -> "DRalgo",
-    "Description" -> "DRalgo is an algorithmic implementation that constructs an effective, dimensionally reduced, high-temperature field theory for generic models.",
-    "Creator" -> "Andreas Ekstedt, Philipp Schicho, Tuomas V.I. Tenkanen",
+    "Name" -> "DRalgo/DRalgo",
+    "Version" -> "1.3.0",
+    "Description" -> "DRalgo constructs an effective, dimensionally reduced, high-temperature field theory for generic models",
+    "WolframVersion" -> "13.+",
+	"Creator" -> "\!\(\*TemplateBox[{RowBox[{\"Andreas\", \" \", \"Ekstedt\"}], {URL[\"https://inspirehep.net/authors/1799400\"], None}, \"https://inspirehep.net/authors/1799400\", \"HyperlinkActionRecycled\", {\"HyperlinkActive\"}, BaseStyle -> {\"Hyperlink\"}, HyperlinkAction -> \"Recycled\"},\"HyperlinkTemplate\"]\), \!\(\*TemplateBox[{RowBox[{\"Philipp\", \" \", \"Schicho\"}], {URL[\"https://inspirehep.net/authors/1639147\"], None}, \"https://inspirehep.net/authors/1639147\", \"HyperlinkActionRecycled\", {\"HyperlinkActive\"}, BaseStyle -> {\"Hyperlink\"}, HyperlinkAction -> \"Recycled\"},\"HyperlinkTemplate\"]\), \!\(\*TemplateBox[{RowBox[{\"Tuomas V.I.\", \" \", \"Tenkanen\"}], {URL[\"https://inspirehep.net/authors/1507627\"], None}, \"https://inspirehep.net/authors/1507627\", \"HyperlinkActionRecycled\", {\"HyperlinkActive\"}, BaseStyle -> {\"Hyperlink\"}, HyperlinkAction -> \"Recycled\"},\"HyperlinkTemplate\"]\)",
     "URL" -> "https://github.com/DR-algo/DRalgo",
+    "Icon" -> "FrontEnd/DRalgo_logo.eps",
     "License" -> "GPL-3.0+",
     "PublisherID" -> "DRalgo",
-    "Version" -> "1.3.0",
+    "Category" -> "Physics",
+    "Keywords" -> {"cosmology","phase transitions","gravitational waves","thermal potential","scalar potential","BSM"},
+    "PrimaryContext" -> "DRalgo`DRalgo`",
     "Extensions" -> {
       {
         "Kernel",
         "Root" -> "Kernel",
-        "Context" -> {
-			"DRalgo`",
-			"DRalgo.m"
-		}
+        "Context" -> {"DRalgo`", "DRalgo.m"}
+      },
+      {
+        "FrontEnd",
+        "Root" -> "FrontEnd"
       }
-    },
-    "MathematicaVersion" -> "13+"
+    }
   |>
 ]
