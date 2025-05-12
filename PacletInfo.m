89d17e8354ffffcc69966b5fc509e095befeffa1
Philipp Schicho <schicho@icloud.com>
2025-05-12 19:02:30 +0200

    new DRalgo banner

--- PacletInfo.m
+++ PacletInfo.m
@@ -4,11 +4,12 @@ PacletObject[
   <|
     "Name" -> "DRalgo/DRalgo",
     "Version" -> "1.3.0",
+    "IsPrereleaseVersion" -> False,
     "Description" -> "DRalgo constructs an effective, dimensionally reduced, high-temperature field theory for generic models",
     "WolframVersion" -> "13.+",
 	"Creator" -> "\!\(\*TemplateBox[{RowBox[{\"Andreas\", \" \", \"Ekstedt\"}], {URL[\"https://inspirehep.net/authors/1799400\"], None}, \"https://inspirehep.net/authors/1799400\", \"HyperlinkActionRecycled\", {\"HyperlinkActive\"}, BaseStyle -> {\"Hyperlink\"}, HyperlinkAction -> \"Recycled\"},\"HyperlinkTemplate\"]\), \!\(\*TemplateBox[{RowBox[{\"Philipp\", \" \", \"Schicho\"}], {URL[\"https://inspirehep.net/authors/1639147\"], None}, \"https://inspirehep.net/authors/1639147\", \"HyperlinkActionRecycled\", {\"HyperlinkActive\"}, BaseStyle -> {\"Hyperlink\"}, HyperlinkAction -> \"Recycled\"},\"HyperlinkTemplate\"]\), \!\(\*TemplateBox[{RowBox[{\"Tuomas V.I.\", \" \", \"Tenkanen\"}], {URL[\"https://inspirehep.net/authors/1507627\"], None}, \"https://inspirehep.net/authors/1507627\", \"HyperlinkActionRecycled\", {\"HyperlinkActive\"}, BaseStyle -> {\"Hyperlink\"}, HyperlinkAction -> \"Recycled\"},\"HyperlinkTemplate\"]\)",
     "URL" -> "https://github.com/DR-algo/DRalgo",
-    "Icon" -> "FrontEnd/DRalgo_logo.eps",
+    "Icon" -> "FrontEnd/logo.png",
     "License" -> "GPL-3.0+",
     "PublisherID" -> "DRalgo",
     "Category" -> "Physics",
