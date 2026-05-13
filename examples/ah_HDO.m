05fd4d82a0c01c7c48efbbf8c0b29208757c1ee0
Philipp Schicho <schicho@icloud.com>
2026-05-14 00:02:18 +0200

    ah model

--- examples/ah_HDO.m
+++ examples/ah_HDO.m
@@ -5,6 +5,7 @@ Quit[];
 
 SetDirectory[NotebookDirectory[]];
 DRalgo`DRalgo`$LoadGroupMath=True;
+DRalgo`DRalgo`$GroupMathMultipleModels=True; (*Put this if you want to create multiple model-files with the same kernel*)
 <<../Kernel/DRalgo.m
 
 
@@ -66,15 +67,27 @@ PerformDRhard[]
 (* ::Section:: *)
 (*Dimension 6 Matching*)
 
-(*The complex scalar field \phi is decomposed in terms of two real scalar field \\phi_1 and \phi_2 as follows \phi=1/Sqrt[2](\phi_1+i\phi_2) which can be written as \phi=V_i \phi_i,
-therefore we can construct all the tensors using the vector V={1,I}/Sqrt[2], its complex conjugate Vs={1,-I}/Sqrt[2] and the projector on the gauge field, which, since it's the only gauge field of the model, it's just id={1}*)
 
+(*
+	The complex scalar field \phi is decomposed in terms of
+	two real scalar field \\phi_1 and \phi_2
+	as follows \phi=1/Sqrt[2](\phi_1+i\phi_2) which can be written
+	as \phi=V_i \phi_i,
+	therefore we can construct all the tensors using
+	the vector V={1,I}/Sqrt[2],
+	its complex conjugate Vs={1,-I}/Sqrt[2], and
+	the projector on the gauge field, which, since it is
+	the only gauge field of the model, it's just id={1}
+*)
 V=(1/Sqrt[2]){1,I}; 
 Vs=(1/Sqrt[2]){1,-I};
 id={1};
 
-(*We define \[Alpha][__] the Wilson coefficients associated to the dimension-6 operator basis defined in arxiv 2503.18904, c[__] are just combination of them*)
-
+(*
+	We define \[Alpha][__] the Wilson coefficients associated to
+	the dimension-6 operator basis defined in arxiv 2503.18904,
+	c[__] are just combination of them
+*)
 Subscript[c, 1]=\[Alpha][ D^2 \[Phi]^4,1]-4\[Alpha][ D^2 \[Phi]^4,2];
 Subscript[c, 2]=-\[Alpha][ D^2 \[Phi]^4,2];
 Subscript[c, 3]=-2 \[Alpha][ D^2 \[Phi]^2 B0^2,2]+\[Alpha][ D^2 \[Phi]^2 B0^2,3];
@@ -83,8 +96,9 @@ Subscript[c, 5]=-2 \[Alpha][ D^2 \[Phi]^2 B0^2,2]-\[Alpha][ D^2 \[Phi]^2 B0^2,1]
 Subscript[c, 6]=-3 \[Alpha][ D^2 B0^4];
 
 
-(*Here we construct the group tensors of the higher dimensional operators*)
-
+(*
+	Here we construct the group tensors of the higher dimensional operators
+*)
 Tens2=4\[Alpha][ \[Phi]^2 F^2]SymmetrizeTensor[TensorProduct[V,Vs,id,id],{{1,2}}];
 Tens3=4\[Alpha][ B0^2 F^2]TensorProduct[id,id,id,id];
 Tens4=4(Subscript[c, 1]SymmetrizeTensor[TensorProduct[V,Vs,V,Vs],{{1,2},{3,4}}]+Subscript[c, 2](SymmetrizeTensor[TensorProduct[V,V,Vs,Vs],{{1,2},{3,4}}]+SymmetrizeTensor[TensorProduct[Vs,Vs,V,V],{{1,2},{3,4}}]));
@@ -102,22 +116,61 @@ Tens15=2\[Alpha][ D^4 \[Phi]^2]Symmetrize[TensorProduct[V,Vs]];
 Tens17=2\[Alpha][ D^4 B0^2]TensorProduct[id,id];
 
 
-TensorList={Tens2,Tens3,Tens4,Tens5,Tens6,Tens7,Tens8,Tens9,Tens10,Tens11,Tens12,Tens13,Tens14,Tens15,Tens17}; (*TensorList is the array of the various group tensors refered to higher dimensional operators*)
-NList={2,3,4,5,6,7,8,9,10,11,12,13,14,15,17}; (*NList is the array that indicate to which operator the group tensors in TensorList refer to*)
-WC = DeleteDuplicates[Cases[TensorList//Normal, \[Alpha][__], \[Infinity]]]; (*WC is the array of the various Wilson Coefficients \[Alpha][...]*)
+(*
+	TensorList is the array of the various group tensors refered to
+	higher dimensional operators
+*)
+TensorList={
+	Tens2,Tens3,Tens4,Tens5,Tens6,Tens7,Tens8,Tens9,Tens10,
+	Tens11,Tens12,Tens13,Tens14,Tens15,Tens17
+};
+(*
+	NList is the array that indicate to which operator
+	the group tensors in TensorList refer to
+*)
+NList={2,3,4,5,6,7,8,9,10,11,12,13,14,15,17};
+(*
+	WC is the array of the various Wilson Coefficients \[Alpha][...]
+*)
+WC = DeleteDuplicates[Cases[TensorList//Normal, \[Alpha][__], \[Infinity]]];
 
 
-sol=Dimension6Matching[TensorList,NList,WC,d][[1]]; (*Dimension6Matching and Dimension5Matching find the values of the Wilson coefficients listed in WC, d is the number of spatial dimensions, Zb and Zf are 1 loop master integral *)
+(*
+	Dimension6Matching and Dimension5Matching find the values of
+	the Wilson coefficients listed in WC,
+	d is the number of spatial dimensions,
+	Zb and Zf are 1 loop master integral
+*)
+sol=Dimension6Matching[TensorList,NList,WC,d][[1]];
 sol//Factor//TableForm
 
-(*The solutions of the Wilson Coefficient is left in generic R-\xi gauge, the user can easily check that this gauge dependence is removed once redundancy of the operator basis is removed as well *)
-(*Zb[3,0] is an Hard Thermal 1 loop Integral, its value in d=3 can be reproduce with the function HardThermal1LoopInt["B",3,0,3]*)
-
-sol=Dimension6Matching[{Tens2},{2},{\[Alpha][\[Phi]^2 F^2]},d][[1]];  (*The matching can be done singularly for each group tensor*)
+(*
+	The solutions of the Wilson Coefficient is
+	left in generic R-\xi gauge,
+	the user can easily check that this gauge dependence
+	is removed once redundancy of the operator basis is removed as well
+	*)
+(*
+	Zb[3,0] is an Hard Thermal 1 loop Integral,
+	its value in d=3 can be reproduce with
+	the function HardThermal1LoopInt["B",3,0,3]
+*)
+(*
+	The matching can be done singularly for each group tensor
+*)
+sol=Dimension6Matching[{Tens2},{2},{\[Alpha][\[Phi]^2 F^2]},d][[1]];
 sol//Factor//TableForm
 
 
-ODIM6[2,d]//MatrixForm (*The functions ODIM6 and ODIM5 return the group tensors of the various operators*)
+(*
+	The functions ODIM6 and ODIM5 return
+	the group tensors of the various operators
+*)
+ODIM6[2,d]//MatrixForm
 
 
-(*The solutions returned by the Dimension6Matching function agree with those reported in arXiv:2503.18904, where the Abelian Higgs model has already been studied*)
+(*
+	The matching results for the Abelian Higgs model
+	returned by the Dimension6Matching function agree with
+	those reported in arXiv:2503.18904
+*)
